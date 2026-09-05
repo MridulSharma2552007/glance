5c0bf38fa082e481ccf669e4e32e3d43bfbb72ba
MridulSharma2552007 <mridul25sharma52007@gmail.com>
2026-09-05 11:18:38 +0530

    Implement initial app structure with routing and theming

---
+++ lib/app/app_router.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class AppRouter {
+  static final router=GoRouter(routes: [
+    GoRoute(path: '/',builder: (context, state) => Scaffold(),)
+  ]);
+}
