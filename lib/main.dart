f7f24d589b63f508c5e44d9289cd5ab7c77fd117
MridulSharma2552007 <mridul25sharma52007@gmail.com>
2026-09-05 12:40:09 +0530

    Fix formatting in main.dart; add newlines for improved readability

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,9 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:glance/app/app.dart';
 
 void main()async{
+
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
+  
   runApp(const App());
 }
